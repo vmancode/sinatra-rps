cccf151dc750b76e6052683752544c77dd4a3abb
vmancode <146589771+vmancode@users.noreply.github.com>
2023-10-21 02:44:59 +0000

    commit1

--- app.rb
+++ app.rb
@@ -5,5 +5,45 @@ get("/") do
   "
   <h1>Welcome to your Sinatra App!</h1>
   <p>Define some routes in app.rb</p>
+  <h1><a href="/">Home</a></h1>
+  <h1><a href="/rock">Refresh</a></h1>
+
   "
+end
+
+
+get("/rock") do
+
+  #myplay = ["rock", "paper", "scissor"]
+  #compplay = ["rock", "paper", "scissor"]
+
+  #myplay1 = myplay.sample
+  #compplay1 = compplay.sample
+
+  #if myplay1 == compplay1
+  #  outcome = "tied"
+  #elsif myplay1 == "rock" && compplay1 == "paper"
+  #  outcome = "lost"
+  #elsif myplay1 == "paper" && compplay1 == "scissor"
+  #  outcome = "lost"
+  #elsif myplay1 == "scissor" && compplay1 == "rock"
+  #  outcome = "lost"  
+  #else
+  #  outcome = "won"
+  #end
+
+
+  moves = ["rock", "paper", "scissor"]
+  @comp_move = moves.sample
+
+  if @comp_move == "rock"
+    @outcome = "tied"
+  elsif @comp_move == "scissor"
+    @outcome = "won"
+  else
+    @outcome  = "lost"
+  end
+
+
+  erb(:zebra)
 end
